af0c85952b2a3d23ff7721067e0fedf3f0c46198
Chike2020 <okechukwuchike@gmail.com>
2026-05-22 19:51:32 -0500

    TEST GREEN: Add compliant non-PHI bucket (should PASS)

---
+++ terraform/test-compliant.tf
@@ -0,0 +1,11 @@
+# TEST: This resource is COMPLIANT with all policies
+# Non-PHI bucket (policies don't apply)
+resource "aws_s3_bucket" "test_compliant" {
+  bucket = "test-public-bucket-${random_id.suffix.hex}"
+
+  tags = {
+    Name      = "test-compliant"
+    DataClass = "public"  # NOT PHI - policies don't apply
+    Purpose   = "policy-test"
+  }
+}
